429372d8ee00c6759fb2c6ee0ffeed462416db41
Jan Max Meyer <jmm@phorward.de>
2019-10-02 14:45:43 +0200

    Restructured branch to work as embedded submodule

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 #-*- coding: utf-8 -*-
 from .utility import parseFloat, parseInt, optimizeValue
-from .logics import Interpreter  #, Parser        (will fail on PyJS at run-time)
+from .logics import Interpreter
 from .parser import ParseException
 from . import vistache
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(
-	name="logics",
-	version="2.5",
-	packages=["logics"]
-)
